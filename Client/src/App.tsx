ba4e7b8d1e38952d3514f75d4e7ab9c29ffe2c8c
Dylan Panepucci <dylanpanepucci@Mac.lan>
2025-03-03 21:47:53 -0500

    styling done to main page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import sunLogo from './assets/367527_pointy_sun_icon.svg'
-import pgaLogo from '/PGA TOUR_idATncDXGY_1.svg'
+import sunLogo from './assets/367527_pointy_sun_icon.svg' // Sun Logo
+import pgaLogo from '/PGA TOUR_idATncDXGY_1.svg' // PGA Logo
+import noteBook from '/notebook.png' // Notebook Logo
+import golfBall from './assets/golf-icon.png'
 import './App.css'
 
 function App() {
@@ -8,26 +10,37 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Check in on the Pros!</h1>
-        <a href="https://www.pgatour.com/" target="_blank">
-          <img src={pgaLogo} className="logo" alt="Vite logo" />
-        </a>
-        <h1>How's the Weather?</h1>
-        <a href="https://challenge-9-weather-app.onrender.com" target="_blank">
-          <img src={sunLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <h1>Welcome to Golf Geek!</h1>
+      <main className="LogoPage">
+        <div>
+          <h2>Check in on the Pros!</h2>
+          <a href="https://www.pgatour.com/" target="_blank">
+            <img src={pgaLogo} className="logo" alt="PGA logo" />
+          </a>
+        </div>
+        <div>
+          <h2>How's the Weather?</h2>
+          <a href="https://challenge-9-weather-app.onrender.com" target="_blank">
+            <img src={sunLogo} className="logo react" alt="Sun logo" />
+          </a>
+        </div>
+        <div>
+          <h2>Log Your Round</h2>
+          <a href="https://www.pgatour.com/" target="_blank">
+            <img src={golfBall} className="logo" alt="Golf Ball logo" />
+          </a>
+        </div>
+        <div>
+          <h2>Previous Rounds</h2>
+          <a href="https://www.pgatour.com/" target="_blank">
+            <img src={noteBook} className="logo" alt="Notebook logo" />
+          </a>
+        </div>
+      </main>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the logos to learn more
       </p>
     </>
   )
